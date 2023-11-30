9e641fde206335990c8802f2e638f17e5a449914
Shizola <tedflb@gmail.com>
2023-11-30 01:04:17 +0000

    agent moving

--- Assets/Scripts/NavMeshBaker.cs
+++ Assets/Scripts/NavMeshBaker.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 using Unity.AI.Navigation;
 using UnityEngine.XR.ARFoundation;
 using TMPro;
+using UnityEngine.AI;
 
 public class NavMeshBaker : MonoBehaviour
 {
     public float updateInterval;
     private float timeSinceLastUpdate = 0.0f;
-    private ARPlane[] _ARPlanes;
     public TextMeshProUGUI text;
+    public ARPlaneManager ARPlaneManager;
+    public NavMeshAgent agent;
 
+    public Transform player;
 
     // Update is called once per frame
     void Update()
     {
-        if(timeSinceLastUpdate > updateInterval)
+        if (timeSinceLastUpdate > updateInterval)
         {
             timeSinceLastUpdate = 0.0f;
             LookForARPlane();
@@ -26,26 +29,43 @@ public class NavMeshBaker : MonoBehaviour
         {
             timeSinceLastUpdate += Time.deltaTime;
         }
-
-        //Debug.Log(timeSinceLastUpdate);
     }
 
     private void LookForARPlane()
     {
-        _ARPlanes = FindObjectsOfType<ARPlane>();
 
-        if(_ARPlanes.Length > 0)
+        foreach (var plane in ARPlaneManager.trackables)
         {
-            NavMeshSurface surface = _ARPlanes[0].GetComponent<NavMeshSurface>();
-            surface.BuildNavMesh();
-            text.text = "ARPlane found " +  _ARPlanes.Length;
+            if (plane.transform.position.y < player.position.y)
+            {
+                plane.GetComponent<NavMeshSurface>().BuildNavMesh();
+            }
+
+            if(agent.enabled == false)
+            {
+                agent.enabled = true;
+            }
+        }
+
+        if (ARPlaneManager.trackables.count > 0)
+        {
+            // text.text = "ARPlane found " + ARPlaneManager.trackables.count;
+
+            string s = "";
+
+            foreach (var plane in ARPlaneManager.trackables)
+            {
+                s += " " + plane.transform.position.y;
+            }
+
+            text.text = s + " " + player.position.y;
         }
         else
         {
             text.text = "No ARPlane found";
         }
     }
-    
+
 }
 
 
